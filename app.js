8efc830e3a56de241dcbc3c5abfecc47316270a4
Malaya Kumar Nayak <malaya.nayak@sap.com>
2020-09-15 23:35:21 +0530

    NodeJs Sample Dockerized

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const router = express.Router();
+
+const path = __dirname + '/views/';
+
+const port = 8080;
+
+// Middleware Function
+router.use((req, res, next) => {
+    console.log('/' + req.method);
+    next();
+});
+
+router.get('/', (req, res) => {
+    res.sendFile(path + 'index.html');
+});
+
+router.get('/sharks', (req, res) => {
+    res.sendFile(path + 'sharks.html');
+});
+
+app.use(express.static(path));
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log('Example app listening on port 8080!')
+})
